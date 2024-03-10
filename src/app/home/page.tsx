e58d8bbc46c39ee71607d52a86b6a6f71c4db57c
Abfahim21 <abrarfahim21@gmail,com>
2024-03-11 04:43:36 +0600

    special section added

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,7 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import Special from "@/components/Special";
 import React from "react";
 
 export default function HomePage() {
@@ -8,6 +9,7 @@ export default function HomePage() {
     <div>
       <Header />
       <Hero />
+      <Special />
       <Footer />
     </div>
   );
